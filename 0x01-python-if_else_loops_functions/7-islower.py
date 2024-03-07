0e45ff8d75bc25d52ccd12b3b9f40d73fba3baa8
Hart-Ijere <iyeowayi@gmail.com>
2024-03-07 09:49:14 +0100

    check for lowercase character

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def islower(c):
+    """ check for a lower case character.
+
+    Args:
+    c(chr) alphabet to be checked.
+    """
+    if ord(c) in range(97,123):
+        print("{} is lower".format(c))
+    else:
+        print("{} is upper".format(c))
