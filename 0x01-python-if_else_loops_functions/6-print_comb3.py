6cbd4349b185432c6c4d655d5747ad7797a43bdf
Hart-Ijere <iyeowayi@gmail.com>
2024-03-16 22:54:03 +0100

    print combination of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for n in range(10):
-    for m in range(i + 1, 10):
+    for m in range(n + 1, 10):
         print("{}{}, ".format(n,m), end='')
-              
-print("{}{}".format(9,0)
