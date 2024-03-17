93e7a4248fe2f173093bea9647f6483032d28f78
Hart-Ijere <iyeowayi@gmail.com>
2024-03-17 06:29:35 +0100

    print combination of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for n in range(10):
     for m in range(n + 1, 10):
         if n == 8 and m == 9:
             print('{}{}'.format(n, m))
-       else:
-           print('{}{},'.format(n, m), end=' ')
+        else:
+            print('{}{},'.format(n, m), end=' ')
