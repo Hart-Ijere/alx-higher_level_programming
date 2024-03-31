9878a2897f251eb17ef1e44d84f13f153b038342
Hart-Ijere <iyeowayi@gmail.com>
2024-03-31 05:06:14 +0100

    create class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module defines a class square
+"""
+
+
+class Square:
+    """
+    This class is a representation of a cla
+    square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new instance.
+
+        Args:
+            size (int, optional): size of
+            square.
+
+        Raises:
+            ValueError: If size is less
+            than 0
+            TypeError: If size is not an
+            integer
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueEeeor("Size must be > 0")
+
+        self.__size = size
+        
