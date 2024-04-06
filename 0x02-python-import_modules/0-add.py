6c44886f61195720434b8f7bd1eb900149c0d983
Hart-Ijere <iyeowayi@gmail.com>
2024-04-06 15:37:10 +0100

    Import a simple function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-
-if not __name__ == "__main__":
-    a = 1
-    b = 2
-    c = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, c)
+a = 1
+b = 2
+c = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, c)
 
