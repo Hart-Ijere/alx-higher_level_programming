cea379342a6d22821908f936da0d76b40ab59df4
Hart-Ijere <iyeowayi@gmail.com>
2024-04-13 22:59:38 +0100

    import a function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from  add_0 import add
+from add_0 import add
 a = 1
 b = 2
 
-if not __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b add(a, b)))
-
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
