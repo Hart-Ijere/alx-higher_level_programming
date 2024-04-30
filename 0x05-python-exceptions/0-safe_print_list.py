d2c0529a0ddccb8ec1345020dace8b401eddcba0
Hart-Ijere <iyeowayi@gmail.com>
2024-04-30 08:39:31 +0100

    print number of elements in a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+def countList(my_list=[]):
+    ''' Count items in a list'''
+    if my_list == '\0':
+        return 0
+    else:
+        count = 0
+        for i in my_list:
+            count += 1
+
+    return count
+
+def safe_print_list(my_list=[], x=0):
+    '''Prints the correct number of elements in a list
+    '''
+    item_cnt = countList(my_list)
+
+    try:
+        for i in my_list:
+            if item_cnt != x:
+                raise ValueError
+            else:
+                print(i, end=' ')
+
+    except ValueError:
+        print("Number passed to x does not match number of items in the list")
+
+    finally:
+        return item_cnt
+    
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,14 @@
+0x05. Python - Exceptions
+
+This directory contains Python Exceptions project tasks at ALX.
+
+It features the following tasks:
+0. Safe list printing
+1. Safe printing of an integers list
+2. Print and count integers
+3. Integers division with debug
+4. Divide a list
+5. Raise exception
+6. Raise a message
+
+It also include four other advanced tasks not listed here.
