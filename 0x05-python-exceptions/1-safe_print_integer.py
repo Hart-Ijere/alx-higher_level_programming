5e528fc12cb9df680c18c01b7c95de3e4e2a77aa
Hart-Ijere <iyeowayi@gmail.com>
2024-04-30 12:17:08 +0100

    print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     if value != '\0':
         try:
             if isinstance(value, int):
                 print("{:d}".format(value))
-
         except TypeError:
             print("{} is not an integer".format(value))
-
         finally:
             if isinstance(value, int):
                 return True
             else:
                 return False
-            
-        
