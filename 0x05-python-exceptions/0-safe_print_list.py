aec9dfb97429301131aaddbf0fdd713e55fd4b47
Hart-Ijere <iyeowayi@gmail.com>
2024-04-30 12:29:51 +0100

    count items in list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
+
 def countList(my_list=[]):
-    ''' Count items in a list'''
-    if my_list == '\0':
+    '''Count items in a list'''
+    if not my_list:
         return 0
     else:
         count = 0
         for i in my_list:
             count += 1
-
     return count
 
 def safe_print_list(my_list=[], x=0):
-    '''Prints the correct number of elements in a list
-    '''
+    '''Prints the correct number of elements in a list'''
     item_cnt = countList(my_list)
 
     try:
@@ -27,4 +26,5 @@ def safe_print_list(my_list=[], x=0):
 
     finally:
         return item_cnt
+
     
