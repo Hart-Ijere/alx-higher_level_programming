6f8f6aef21f841c4036c1e80e53e8da7a4646460
Hart-Ijere <iyeowayi@gmail.com>
2024-04-30 12:37:12 +0100

    print number of items in list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
 
-def countList(my_list=[]):
-    '''Count items in a list'''
-    if not my_list:
-        return 0
-    else:
-        count = 0
-        for i in my_list:
-            count += 1
-    return count
-
 def safe_print_list(my_list=[], x=0):
-    '''Prints the correct number of elements in a list'''
-    item_cnt = countList(my_list)
-
+    '''Prints x elements of a list'''
     try:
-        for i in my_list:
-            if item_cnt != x:
-                raise ValueError
+        count = 0
+        for item in my_list:
+            if count < x:
+                print(item, end=' ')
+                count += 1
             else:
-                print(i, end=' ')
-
-    except ValueError:
-        print("Number passed to x does not match number of items in the list")
+                break  # Stop printing once x elements are printed
+    except TypeError:
+        print("An error occurred while printing the list")
 
     finally:
-        return item_cnt
-
-    
+        return count
