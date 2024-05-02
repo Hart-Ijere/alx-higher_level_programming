c01a9a9f9be90e528bd9a54d51fe42645fd318aa
Hart-Ijere <iyeowayi@gmail.com>
2024-05-03 00:39:58 +0100

    save print enerf

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    myResult = 0
+    try:
+        if not isinstance((a, b), int):
+            raise TypeError
+    except TypeError:
+        print("Ooops! yoi entered an invalid number")
+    except ZeroDivisionError:
+        print("a and b must be integers")
+    finally:
+        if b > 0:
+            myResult = a / b
+            print("Inside reseult: {}".format(myResult))
+            return myResult
+        else;
+            myWord = "None"
+            print("Inside result: {}".format(myWord)
+            return None
+        
