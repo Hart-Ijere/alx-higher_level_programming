b507ec8c95b37d31a0cd219f1d060c37e9ec109a
Hart-Ijere <iyeowayi@gmail.com>
2024-05-02 06:26:51 +0100

    print x elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    for i in my_list:
-        print(i, end=' ')
-    return x
+    try:
+        if isinstance(x, int)
+        return x
+    except ValueError:
+        print("x is not an integer")
+    except TypeError:
+        print("x is not an integer")
+    finally:
+        for i in my_list:
+            print(i)
 
