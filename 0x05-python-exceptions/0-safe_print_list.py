74ea5c3aff6a59fbfc24704bdb59a2072076f5dd
Hart-Ijere <iyeowayi@gmail.com>
2024-05-02 07:42:02 +0100

    print elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,13 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        if isinstance(x, int)
-        return x
-    except ValueError:
-        print("x is not an integer")
-    except TypeError:
-        print("x is not an integer")
-    finally:
-        for i in my_list:
-            print(i)
+        if isinstance(x, int):
+            for i in my_list:
+                print(i, end='')
+            print()  # Print a newline after printing all elements
+            return len(my_list)  # Return the length of my_list
+        else:
+            raise ValueError("x is not an integer")
+    except ValueError as e:
+        print(e)  # Print the error message if x is not an integer
 
