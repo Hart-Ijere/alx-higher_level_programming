d4bdc8ebd8c4f120ef27f1158913ba721d5c5890
Hart-Ijere <iyeowayi@gmail.com>
2024-05-04 08:36:25 +0100

    print items in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count_item = 0
-    
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             count_item += 1
     except IndexError:
-        print("\nOnly", count_items, "elements were printed because the list is shorter.")
+        print("\nOnly", count_item)
     print()
-    return count_item 
-
-
+    return count_item
