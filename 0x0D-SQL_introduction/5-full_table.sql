5202815c968538236b6b276cd680e95c10f6abf0
Hart-Ijere <iyeowayi@gmail.com>
2024-05-21 23:31:08 +0100

    print a full description of the database

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Task: Print the full description of the table first_table from the database hbtn_0c_0
+-- A query that retrieves the full description of the table first_table.
+
+SHOW COLUMNS FROM hbtn_0c_0.first_table;
+
