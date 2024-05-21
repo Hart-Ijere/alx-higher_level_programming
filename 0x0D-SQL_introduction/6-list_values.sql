c76cdb61bcaa1ab7ac7bc611f552d9a86a1a7c9a
Hart-Ijere <iyeowayi@gmail.com>
2024-05-21 23:46:36 +0100

    print all fields

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Task: List all rows of the table first_table from the database hbtn_0c_0
+-- This is a query that selects all fields from all rows in the table first_table.
+
+SELECT * FROM hbtn_0c_0.first_table;
