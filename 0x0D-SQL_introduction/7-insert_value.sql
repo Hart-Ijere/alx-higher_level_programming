22d4483369a6c0ff2ad8cc703006ab9dd2d086f9
Hart-Ijere <iyeowayi@gmail.com>
2024-05-21 23:55:12 +0100

    insert in a new...

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Task: Insert a new row in the table first_table from the database hbtn_0c_0
+-- This query inserts a new row with id = 89 and name = 'Best School'.
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
