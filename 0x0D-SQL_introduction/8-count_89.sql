85b360d32a155190e5bd64a34bd88bafa6c17344
Hart-Ijere <iyeowayi@gmail.com>
2024-05-22 00:00:35 +0100

    display the number of record

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Task: Display the number of records with id = 89 in the table first_table from the database hbtn_0c_0
+-- This query counts the number of records with id = 89.
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
