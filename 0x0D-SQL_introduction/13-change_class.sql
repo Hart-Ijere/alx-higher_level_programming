3e281a8bef584a5ff8e047338fb3f0ba1cb65f43
Hart-Ijere <iyeowayi@gmail.com>
2024-05-22 07:44:04 +0100

    remove certain records

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Task: Remove all records with a score <= 5 in the table second_table from the database hbtn_0c_0
+-- A query that deletes all rows where the score is less than or equal to 5.
+
+DELETE FROM second_table WHERE score <= 5;
