d92e0678352b741104a6b1c4eaf28b42870dd980
Hart-Ijere <iyeowayi@gmail.com>
2024-05-22 07:40:13 +0100

    update score

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Task: Update the score of Bob to 10 in the table second_table from the database hbtn_0c_0
+-- A query that updates the score to 10 for the row where the name is 'Bob'.
+
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
