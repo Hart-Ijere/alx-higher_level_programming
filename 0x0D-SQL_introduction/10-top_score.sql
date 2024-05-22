0017b84846ad77e3f8d388d81f95c4bf0ecd9b92
Hart-Ijere <iyeowayi@gmail.com>
2024-05-22 07:20:33 +0100

    list all records of a table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Task: List all records of the table second_table from the database hbtn_0c_0
+-- A query that selects and displays the score and name from all rows in second_table, ordered by score in descending order.
+
+SELECT score, name FROM second_table ORDER BY score DESC;
