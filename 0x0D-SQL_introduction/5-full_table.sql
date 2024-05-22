18b1c9080e929d09a4a8fff400021eac226f33f7
Hart-Ijere <iyeowayi@gmail.com>
2024-05-22 08:06:25 +0100

    retrieve full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
 -- Task: Print the full description of the table first_table from the database hbtn_0c_0
--- A query that retrieves the full description of the table first_table.
+-- A query that retrieves the full description of the table first_table using the information_schema.
 
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+
