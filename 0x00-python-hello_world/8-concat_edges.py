f96f476ffed6833838c9a0cbadda186a1e015645
Hart-Ijere <iyeowayi@gmail.com>
2024-06-09 20:01:25 +0100

    slice strings

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[6:7] + str[105:111] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str1 = str[39:66]
+str2 = str[6:7]
+str3 = str[107:112]
+str4 = str[:6]
+print(str1 + str2 + str3 + str4)
