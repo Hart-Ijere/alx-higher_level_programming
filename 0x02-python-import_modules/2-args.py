178b8ebd21685333475d191db55d636fa9a910b1
Hart-Ijere <iyeowayi@gmail.com>
2024-06-10 09:36:50 +0100

    print and list arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(argc))
         for n in range(1, argc + 1):
-            print("{}: {}".format(n, argv[i]))
+            print("{}: {}".format(n, argv[n]))
