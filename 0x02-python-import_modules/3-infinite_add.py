8c4ae0b7a11f2a77232b4b0879736ed7193c3914
Hart-Ijere <iyeowayi@gmail.com>
2024-06-10 10:51:30 +0100

    add all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
 
     total = 0
     for n in range(len(sys.argv) - 1):
-        total += int(sys.argv[+ 1])
+        total += int(sys.argv[n + 1])
     print("{}".format(total))
