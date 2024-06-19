1b55ca5a0fcda8c21f8207d879577286bdbc4f5a
Hart-Ijere <iyeowayi@gmail.com>
2024-06-20 00:46:09 +0100

    print two arguments passed

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Retrieve the first and second arguments passed
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Print the output in the format: "<arg1> is <arg2>"
+console.log(`${arg1} is ${arg2}`);
