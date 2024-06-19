8cdaa9eaddae6cc0cfc96b6c3ee8b0e09caa71b9
Hart-Ijere <iyeowayi@gmail.com>
2024-06-19 21:51:04 +0100

    print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myFirstVar = 'C is fun';
+const mySecVar = 'Python is cool';
+const myThirdVar = 'JavaScript is amazing';
+console.log(myFirstVar);
+console.log(mySecVar);
+console.log(myThirdVar);
