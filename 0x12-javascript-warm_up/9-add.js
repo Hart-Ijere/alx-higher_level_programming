7d436e2e1d1a5e08d61852661c8eb437f1f66c9e
Hart-Ijere <iyeowayi@gmail.com>
2024-06-20 05:21:39 +0100

    print the addition of two integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Define the add function
+function add(a, b) {
+  return a + b;
+}
+
+// Retrieve and convert the first and second arguments passed to the script
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+
+// Check if the arguments are valid numbers and print the result of the addition
+if (isNaN(a) || isNaN(b)) {
+  console.log("NaN");
+} else {
+  console.log(add(a, b));
+}
