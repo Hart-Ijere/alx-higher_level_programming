593da9a1c21f71c8b6a384b0271026df73056a02
Hart-Ijere <iyeowayi@gmail.com>
2024-06-28 00:29:55 +0100

    include definition of states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """
-Start link class to table in database.
+This module defines a State class and a Base instance
+for working with MySQL and SQLAlchemy.
 """
-import sys
-from sqlalchemy import create_engine
-from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ), pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that links to the MySQL table states.
+
+    Attributes:
+        id (int): The state's id.
+        name (str): The state's name.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
