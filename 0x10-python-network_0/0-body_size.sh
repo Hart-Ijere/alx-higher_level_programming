6f9c75d20b90b0aa494f8a0df58c9a41e0e3229e
Hart-Ijere <iyeowayi@gmail.com>
2024-07-04 23:21:28 +0100

    take a URL...

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-# This script takes a URL, sends a request, and displays the size of the body of the response.
-curl -s -o /dev/null -w "%{size_download}\n" "$1"
+#!/bin/bash
+# This script takes a URL, sends a request, and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
