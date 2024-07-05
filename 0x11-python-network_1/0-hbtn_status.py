32f9331a0b278cec9b46ad62d2b77cb6fd422e40
Hart-Ijere <iyeowayi@gmail.com>
2024-07-06 00:13:27 +0100

    fetch status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,17 +4,19 @@
 
 from urllib.request import Request, urlopen
 
+
 def fetch_url(url):
     """Fetches content from the given URL and returns it."""
     req = Request(url)
     with urlopen(req) as res:
         return res.read()
 
+
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     content = fetch_url(url)
     utf8_content = content.decode('utf-8')
-    
+
     print('Body response:')
     print('\t- type: {_type}'.format(_type=type(content)))
     print('\t- content: {_content}'.format(_content=content))
