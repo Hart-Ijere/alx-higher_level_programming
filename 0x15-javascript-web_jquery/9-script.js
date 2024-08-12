ff8b4b6d1c34e4b43c318a7ba3f974eea1b7abcc
Hart-Ijere <iyeowayi@gmail.com>
2024-08-12 23:58:16 +0100

    fetch and display the value hello

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fetch Hello Translation</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-      $(document).ready(() => {
+$(document).ready(() => {
         // Use jQuery to fetch the translation for "hello"
         $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', (data) => {
           // Update the text of the <div> element with the translation
           $('#hello').text(data.hello)
         })
       })
-    </script>
-</head>
-<body>
-    <div id="hello"></div>
-</body>
-</html>
