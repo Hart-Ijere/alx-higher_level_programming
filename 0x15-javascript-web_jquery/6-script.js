16f0304aa31294ad3f3cd9fae164b8ff77474ec9
Hart-Ijere <iyeowayi@gmail.com>
2024-08-12 22:26:43 +0100

    update the text of the header element

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// Use jQuery to select the DIV with the ID 'update_header' and bind a click event handler
+$('#update_header').click(() => {
+  // Update the text of the <header> element to "New Header!!!"
+  $('header').text('New Header!!!')
+})
