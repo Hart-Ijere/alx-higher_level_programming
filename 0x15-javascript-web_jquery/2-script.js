7f24bd55e0aa65a2b29ecb3585bdb0e77af726b2
Hart-Ijere <iyeowayi@gmail.com>
2024-08-12 22:09:24 +0100

    update the text color if the header

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,6 @@
-let $headerElem = $('header');
-let $divRedHeader = $('div#red_header');
+//  Use jQuery to select the DIV with the ID 'red_header' and bind a click event handler
+$('#red_header').click(() => {
+  // When the DIV is clicked, update the color of the header element to red (#FF0000)
+  $('header').css('color', '#FF0000')
+})
 
-$divRedHeader.on('click', function () {
-  $headerElem.css('color', '#FF0000');
-});
